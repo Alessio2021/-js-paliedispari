ba7af9cb45280731331b3f725467f68f1830e4ef
Alessio2021 <alessiobut1911@gmail.com>
2021-11-29 15:52:36 +0100

    function palindromo

--- js/script.js
+++ js/script.js
@@ -4,15 +4,31 @@ const word = prompt('Inserisci nome')
 // Creare una funzione per capire se la parola inserita è palindroma
 // (prima senza funzione e poi con funzione)
 
-const wordSplit = word.split('');
-// console.log(wordSplit);
-const wordReverse = wordSplit.reverse('');
-// console.log(wordReverse);
-const finalWord = wordReverse.join('');
-// console.log(finalWord);
+// const wordSplit = word.split('');
+// // console.log(wordSplit);
+// const wordReverse = wordSplit.reverse('');
+// // console.log(wordReverse);
+// const finalWord = wordReverse.join('');
+// // console.log(finalWord);
 
-if (word == finalWord) {
-    console.log('palindromo');
-} else {
-    console.log('non palindromo');
+// if (word == finalWord) {
+//     console.log('palindromo');
+// } else {
+//     console.log('non palindromo');
+// }
+
+function palindromo(num) {
+    const wordSplit = word.split('');
+    const wordReverse = wordSplit.reverse('');
+    const finalWord = wordReverse.join('');
+
+    if (word == finalWord) {
+        console.log('palindromo');
+    } else {
+        console.log('non palindromo');
+    }
+    
+    return finalWord;
 }
+
+const wordPalindromo = palindromo(word);
