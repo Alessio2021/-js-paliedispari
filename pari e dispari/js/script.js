805fd99412a16cd37eabfbd1c55e3f38a2f51e26
Alessio2021 <alessiobut1911@gmail.com>
2021-11-29 16:17:21 +0100

    even odd

---
+++ pari e dispari/js/script.js
@@ -0,0 +1,27 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+const evenOdd = prompt('scegli tra pari o dispari');
+const number = parseInt(prompt('scegli un numero'));
+
+// Generiamo un numero random(sempre da 1 a 5) per il computer
+
+const numbComputer = Math.round(Math.random() * 5);
+console.log(numbComputer);
+// Sommiamo i due numeri
+
+const sum = number + numbComputer;
+
+// Stabiliamo se la somma dei due numeri è pari o dispari
+let find = false;
+
+if (sum % 2 == 0) {
+    console.log('pari');
+} else {
+    console.log('dispari');
+}
+
+// Dichiariamo chi ha vinto
+
+if (  evenOdd ) {
+    
+}
